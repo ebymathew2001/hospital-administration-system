a64e6c094bb21f4b912b5fbd3ec13ffb126e72b6
Eby Mathew <ebykannamkulathu@gmail.com>
2025-04-05 23:29:01 +0530

    started with spring security

---
+++ src/main/java/com/example/Hospital/Administration/System/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.Hospital.Administration.System.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/RECEPTIONIST/**").hasRole("RECEPTIONIST")
+                        .requestMatchers("/DOCTOR/**").hasRole("DOCTOR")
+                        .requestMatchers("/PHARMACIST/**").hasRole("PHARMACIST")
+                        .requestMatchers("/login","/register", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
